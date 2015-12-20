2b9be142e92b98f60a5450c123bb1641f955976d
Vladimir Seleznov <v.e.seleznov@gmail.com>
2015-12-20 14:13:37 +0200

    level 13 solutions

--- src/com/javarush/test/level13/lesson02/task08/Solution.java
+++ src/com/javarush/test/level13/lesson02/task08/Solution.java
@@ -13,4 +13,14 @@ public class Solution
     public static void main(String[] args) throws Exception
     {
     }
+
+    interface Person
+    {
+        boolean isAlive();
+    }
+
+    interface Presidentable extends Person
+    {
+
+    }
 }
