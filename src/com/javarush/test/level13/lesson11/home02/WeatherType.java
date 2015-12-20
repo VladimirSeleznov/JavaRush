9c303dd3af8e935f31cdd93af4bbf4e6b96ebddb
Vladimir Seleznov <v.e.seleznov@gmail.com>
2015-12-20 16:20:30 +0200

    level 13 solutions

--- src/com/javarush/test/level13/lesson11/home02/WeatherType.java
+++ src/com/javarush/test/level13/lesson11/home02/WeatherType.java
@@ -1,6 +1,6 @@
 package com.javarush.test.level13.lesson11.home02;
 
-public interface WeatherType
+public interface WeatherType extends Weather
 {
     String CLOUDY = "Cloudy";
     String FOGGY = "Foggy";
