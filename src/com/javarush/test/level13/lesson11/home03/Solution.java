aa22e3fde226c9a9136de0522b02075c5880e3f5
Vladimir Seleznov <v.e.seleznov@gmail.com>
2015-12-20 17:06:21 +0200

    level 13 solutions

--- src/com/javarush/test/level13/lesson11/home03/Solution.java
+++ src/com/javarush/test/level13/lesson11/home03/Solution.java
@@ -6,10 +6,21 @@ package com.javarush.test.level13.lesson11.home03;
 3. Не забыть освободить ресурсы. Закрыть поток чтения с файла и поток ввода с клавиатуры.
 */
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Solution
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws FileNotFoundException
     {
-        //add your code here
+        Scanner scanner = new Scanner(System.in);
+        String file = scanner.nextLine();
+        scanner.close();
+        Scanner fileScanner = new Scanner(new FileReader(file));
+        while (fileScanner.hasNext())
+        {
+            System.out.println(fileScanner.next());
+        }
     }
 }
--- src/com/javarush/test/level13/lesson11/home04/Solution.java
+++ src/com/javarush/test/level13/lesson11/home04/Solution.java
@@ -6,9 +6,33 @@ package com.javarush.test.level13.lesson11.home04;
 3. Вывести абсолютно все введенные строки в файл, каждую строчку с новой стороки.
 */
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Solution
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException
     {
+        Scanner scanner = new Scanner(System.in);
+        String file = scanner.nextLine();
+        ArrayList<String> strings = new ArrayList<String>();
+        while (true)
+        {
+            String string = scanner.nextLine();
+            strings.add(string);
+            if (string.equals("exit"))
+            {
+                scanner.close();
+                break;
+            }
+        }
+        PrintWriter writer = new PrintWriter(file);
+        for (String s : strings)
+        {
+            writer.println(s);
+        }
+        writer.close();
     }
 }
