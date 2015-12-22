f8ac71aa58c21018515b0602a35713d32b26e7c5
Vladimir <vladimir_seleznov@gmail.com>
2015-12-22 19:00:45 +0200

    level 14 solutions

--- src/com/javarush/test/level14/lesson08/home10/Solution.java
+++ src/com/javarush/test/level14/lesson08/home10/Solution.java
@@ -25,44 +25,47 @@ import java.util.List;
 - учти, что для объекта типа Double нужно вывести "Is double value infinite? " + [Double_object].isInfinite().
 */
 
-public class Solution
-{
-    public static void main(String[] args)
-    {
+public class Solution {
+    public static void main(String[] args) {
         new NotIncapsulatedClass();
     }
 
-    public static class NotIncapsulatedClass
-    {
-        public NotIncapsulatedClass()
-        {
+    public static class NotIncapsulatedClass {
+
+        public NotIncapsulatedClass() {
+
             List<Number> list = new LinkedList<Number>();
-            //1
-            list.add(new Double(1000f));
-            list.add(new Double("123e-445632"));
-            list.add(new Float(-90 / -3));
-            list.remove(new Double("123e-445632"));
+            initList(list);
+            printListValues(list);
+            processCastedObjects(list);
+        }
 
-            //2 - Исправь 2 ошибки
-            for (int i = 0; i <= list.size(); i--)
-            {
-                System.out.println(list.get(i));
-            }
-
-            //3
-            for (Number object : list)
-            {
-                //Исправь 2 ошибки
-                if (object instanceof Float)
-                {
-                    Double a = (Double) object;
-                    System.out.println("Is float value defined? " + a.isNaN());
-                } else if (object instanceof Double)
-                {
+        private void processCastedObjects(List<Number> list) {
+            for (Number object : list) {
+                if (object instanceof Float) {
                     Float a = (Float) object;
+                    System.out.println("Is float value defined? " + a.isNaN());
+                } else if (object instanceof Double) {
+                    Double a = (Double) object;
                     System.out.println("Is double value infinite? " + a.isInfinite());
                 }
             }
+        }
+
+        private void printListValues(List<Number> list) {
+            for (Number number : list) {
+                System.out.println(number);
+            }
+        }
+
+        private List initList(List<Number> list) {
+            list.add((double) 1000f);
+            Float l = new Float("123e-445632");
+            list.add(l);
+            list.add((float) (-90 / -3));
+            list.remove(l);
+
+            return list;
         }
     }
 }
