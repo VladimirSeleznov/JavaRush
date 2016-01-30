38722be245b7a30147e35355c71f0a58ef886c0c
Vladimir <vladimir_seleznov@gmail.com>
2016-01-30 14:51:06 +0200

    level 18 solutions

--- src/com/javarush/test/level18/lesson03/task02/Solution.java
+++ src/com/javarush/test/level18/lesson03/task02/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.test.level18.lesson03.task02;
 
 import java.io.FileInputStream;
+import java.util.Scanner;
 
 /* Минимальный байт
 Ввести с консоли имя файла
@@ -10,5 +11,23 @@ import java.io.FileInputStream;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+        String fileName = scanner.nextLine();
+        scanner.close();
+
+        FileInputStream fileInputStream = new FileInputStream(fileName);
+
+        int fileByte = fileInputStream.read();
+
+        while (fileInputStream.available() > 0) {
+            int currentByte = fileInputStream.read();
+            if (fileByte > currentByte) {
+                fileByte = currentByte;
+            }
+        }
+
+        fileInputStream.close();
+        System.out.println(fileByte);
     }
 }
--- src/com/javarush/test/level18/lesson03/task03/Solution.java
+++ src/com/javarush/test/level18/lesson03/task03/Solution.java
@@ -1,6 +1,10 @@
 package com.javarush.test.level18.lesson03.task03;
 
 import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Scanner;
 
 /* Самые частые байты
 Ввести с консоли имя файла
@@ -11,5 +15,47 @@ import java.io.FileInputStream;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
+
+        HashMap<Integer, Integer> integerHashMap = new HashMap<>();
+
+        Scanner scanner = new Scanner(System.in);
+        String fileName = scanner.nextLine();
+        scanner.close();
+
+        FileInputStream fileInputStream = new FileInputStream(fileName);
+
+        while (fileInputStream.available() > 0) {
+            int currentByte = fileInputStream.read();
+
+            if (!integerHashMap.containsKey(currentByte)) {
+                integerHashMap.put(currentByte, 1);
+            } else {
+                integerHashMap.put(currentByte, integerHashMap.get(currentByte) + 1);
+            }
+        }
+
+        fileInputStream.close();
+
+        int maxByte = 0;
+        int maxCount = 0;
+
+        for (Map.Entry<Integer, Integer> entry : integerHashMap.entrySet()) {
+
+            if (entry.getValue() > maxCount) {
+                maxByte = entry.getKey();
+                maxCount = entry.getValue();
+            }
+        }
+
+        System.out.print(maxByte);
+
+
+        for (Map.Entry<Integer, Integer> entry : integerHashMap.entrySet()) {
+            if (Objects.equals(entry.getValue(), integerHashMap.get(maxByte))) {
+                if (!entry.getKey().equals(maxByte)) {
+                    System.out.print(" " + entry.getKey());
+                }
+            }
+        }
     }
 }
